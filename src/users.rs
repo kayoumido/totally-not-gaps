2b7aa645e44da5efeac0d24340227a871d6cccf4
Doran Kayoumi <dorankayoumi@gmail.com>
2021-06-24 12:32:40 +0200

    Add test for users

--- src/users.rs
+++ src/users.rs
@@ -24,3 +24,47 @@ fn _get_student(username: &str, repository: &dyn UserRepository) -> Result<User,
 
     Ok(u)
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::db::models::User;
+    use crate::db::repository::MockPostgrSQLUserRepository;
+    use crate::errors::{DBError, UserError};
+
+    #[test]
+    fn test_get_unknown_student() {
+        let mut mock = MockPostgrSQLUserRepository::new();
+
+        mock.expect_get_user()
+            .returning(|_| Err(DBError::UserNotFound));
+
+        let res = _get_student("whoami", &mock);
+
+        assert_eq!(Err(UserError::StudentNotFound), res);
+    }
+
+    #[test]
+    fn test_get_student_with_teacher() {
+        let mut mock = MockPostgrSQLUserRepository::new();
+
+        mock.expect_get_user()
+            .returning(|_| Ok(User::new("teach", "hashedPassword", "Teacher")));
+
+        let res = _get_student("teach", &mock);
+
+        assert_eq!(Err(UserError::StudentNotFound), res);
+    }
+
+    #[test]
+    fn test_get_student() {
+        let mut mock = MockPostgrSQLUserRepository::new();
+
+        mock.expect_get_user()
+            .returning(|_| Ok(User::new("student", "hashedPassword", "Student")));
+
+        let res = _get_student("student", &mock);
+
+        assert_eq!(Ok(User::new("student", "hashedPassword", "Student")), res);
+    }
+}
